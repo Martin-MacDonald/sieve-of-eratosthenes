efff919f44f6256f64e5a535f29b81d6c76727f3
Martin MacDonald <martinmacdonald@swarmonline.com>
2019-01-22 10:02:15 +0000

    Updated tests and readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# sieve-of-eratosthenes
+# Sieve of Eratosthenes
 Calculates all the prime numbers between 2 and the given input
+
+## Use
+```sh
+$ npm install --save sieve-of-eratosthenes
+```
+```js
+const primeCalculator = require('sieve-of-eratosthenes');
+primeCalculator(10).then((primes) => {
+  console.log(primes); // [2, 3, 5, 7]
+});
+```
+
+## Author
+Martin Bolton MacDonald
+- [github/martin-macdonald](https://github.com/Martin-MacDonald)
+
+## Licence
+Copyright © 2019, Martin Bolton MacDonald. Released under the MIT [License](LICENSE).
+
+
--- test.js
+++ test.js
@@ -1,8 +1,22 @@
 const primeCalculator = require('.');
 
-test(`Expect 30 to return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]`, () => {
+test('Expect 10 to return [2, 3, 5, 7]', () => {
+  return primeCalculator(10).then((primes) => {
+    expect(primes).toHaveLength(4);
+    expect(primes).toEqual([2, 3, 5, 7]); 
+  });
+});
+
+test('Expect 30 to return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]', () => {
   return primeCalculator(30).then((primes) => {
     expect(primes).toHaveLength(10);
     expect(primes).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]); 
+  });
+});
+
+test('Expect 100 to return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]', () => {
+  return primeCalculator(100).then((primes) => {
+    expect(primes).toHaveLength(25);
+    expect(primes).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]); 
   });
 });
